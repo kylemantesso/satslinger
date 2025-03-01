9abaab7d92294d33948bc06884797b5e77e4d1cd
Kyle Mantesso <kylemantesso@gmail.com>
2025-03-01 19:06:06 +1100

    ai url

--- web/src/app/config.ts
+++ web/src/app/config.ts
@@ -3,7 +3,7 @@ import { DEPLOYMENT_URL } from "vercel-url";
 const ACCOUNT_ID = process.env.ACCOUNT_ID;
 
 // Set the plugin url in order of BITTE_CONFIG, env, DEPLOYMENT_URL (used for Vercel deployments)
-const PLUGIN_URL = DEPLOYMENT_URL || `${process.env.NEXT_PUBLIC_HOST || 'localhost'}:${process.env.PORT || 3000}`;
+const PLUGIN_URL = (DEPLOYMENT_URL || `${process.env.NEXT_PUBLIC_HOST || 'localhost'}:${process.env.PORT || 3000}`) + '/api/ai-plugin';
 
 if (!PLUGIN_URL) {
   console.error(
